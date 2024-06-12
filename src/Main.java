51116046a42a5f2f03841e67ea6c29c916c4f39d
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-06-12 10:14:22 +0200

    clarified author name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 
+/*
+Created by Ivan Savenko. Most functionality implemented 11.06
+*/
 public class Main {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
